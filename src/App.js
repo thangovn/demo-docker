4542290346af280c9ea02c29c1e8963a2bef9b8a
Thuan <tizou777@gmail.com>
2022-06-03 13:00:00 +0700

    add

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>srcws/App.js</code> and sssave to reload.
+          Edit <code>/App.js</code> and sssave to reload.
         </p>
         <a
           className="App-link"
