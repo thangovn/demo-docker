3ea527510707f40329ba72cad35df1fcacbf1cc8
Thuan <tizou777@gmail.com>
2022-06-17 17:12:02 +0700

    add

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>/Paul chym toß</code> and sssave to reload.
-        </p>
+        <p>CAt</p>
         <a
           className="App-link"
           href="https://reactjs.org"
