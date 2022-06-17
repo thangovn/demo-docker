9d3d5b90e0905a3b20f98046e1edfd20057d45d5
Thuan <tizou777@gmail.com>
2022-06-17 17:24:59 +0700

    q

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>CAt DOG</p>
+        <p>CAt DOG BIRD</p>
         <a
           className="App-link"
           href="https://reactjs.org"
